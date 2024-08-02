0f8e9a30e868ae04bdaf19b3aa5d1cde01eb3f71
jooya38 <hyj0102809@gmail.com>
2024-08-02 16:26:03 +0900

    ADD: user, review table(DB)

--- data/create_db.py
+++ data/create_db.py
@@ -3,15 +3,37 @@ import csv
 
 # 데이터베이스 연결
 conn = sqlite3.connect('site.db')
-c = conn.cursor()
+cur = conn.cursor()
 
 # 테이블 생성
-c.execute('''
+# 사기사이트링크(링크(PK))
+cur.execute('''
     CREATE TABLE IF NOT EXISTS sites (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        link TEXT PRIMARY KEY,
+    )
+''')
+
+# 회원(회원번호, 이름, 핸드폰번호, 이메일, 비밀번호, 등록일, 포인트)
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        email TEXT,
+        password TEXT,
+        registration_date TEXT,
+        points INTEGER
+    )
+''')
+
+# 리뷰(회원번호, 링크, 리뷰)
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS reviews (
+        user_id INTEGER,
         link TEXT,
         review TEXT,
-        site_id TEXT
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (link) REFERENCES sites(link)
     )
 ''')
 
@@ -20,10 +42,11 @@ with open('site.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     next(reader)  # 헤더 건너뛰기
     for row in reader:
-        c.execute('''
-            INSERT INTO sites (link, review, site_id)
-            VALUES (?, ?, ?)
-        ''', (row[0], "", ""))
+        cur.execute('''
+            INSERT INTO sites (link)
+            VALUES (?)
+        ''', (row[0],))
+        print(row)
 
 # 변경사항 저장 및 데이터베이스 연결 종료
 conn.commit()
